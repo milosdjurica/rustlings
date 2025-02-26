84e1da94eb05a124e03a60eadf93c989972b11d9
Milos Djurica <milos.djurica@mvpworkshop.co>
2025-02-26 09:22:03 +0100

    quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,18 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        //
+
+        input
+            .iter()
+            .map(|i| match i.1 {
+                Command::Append(n) => format!("{}{}", i.0, "bar".repeat(n)),
+                Command::Trim => i.0.trim().to_string(),
+                Command::Uppercase => i.0.to_uppercase().to_string(),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -36,6 +47,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::my_module::transformer;
+
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
